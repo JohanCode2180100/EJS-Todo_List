2f86678a144229ae76a24d97216a6d54556b7930
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-04-02 21:26:30 +0200

    add test view.ejs. add task for testing app

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const { config } = require("./config");
-
 const app = express();
+
+const tasks = [
+  {
+    title: "apprendre a programmer",
+    done: false,
+  },
+  { title: "Faire les courses", done: true },
+  { title: "bricoler", done: true },
+];
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("todoList");
+  res.render("todoList", { tasks: tasks });
 });
 
 app.listen(config.port, () => {
