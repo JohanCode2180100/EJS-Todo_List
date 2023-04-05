e2d5421e1f17aeed5e2a0445e900148da73db260
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-04-05 08:45:55 +0200

    app complete

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 const express = require("express");
 const { config } = require("./config");
 const app = express();
+const session = require("express-session");
 
-const tasks = [
-  {
-    title: "apprendre a programmer",
-    done: false,
-  },
-  { title: "Faire les courses", done: true },
-  { title: "bricoler", done: true },
-];
+const tasks = [];
 // add express encoded for req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("todoList", { tasks: tasks });
-});
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.get("/task/:id/done", (req, res) => {
-  tasks[req.params.id].done = true;
-  res.redirect("/");
+app.get("/", (req, res) => {
+  if (!req.session.tasks) {
+    req.session.tasks = [];
+  }
+  res.render("todoList", { tasks: req.session.tasks });
 });
 
 app.post("/task", (req, res) => {
-  tasks.push({
-    title: req.body.task,
-    done: false,
-  });
+  if (req.body.task) {
+    req.session.tasks.push({
+      title: req.body.task,
+      done: false,
+    });
+  }
+  res.redirect("/");
+});
+
+app.get("/task/:id/done", (req, res) => {
+  if (req.params.id) {
+    req.session.tasks[req.params.id].done = true;
+    // Your code to mark the task with the specified ID as done
+  }
+  res.redirect("/");
+});
+
+app.get("/task/:id/delete", (req, res) => {
+  req.session.tasks.splice(req.params.id, 1);
   res.redirect("/");
 });
 
